b46f33581ee268dd9b0f32dfbcbe546a3b884003
a.elwardi <a.elwardi98gmail.Com>
2022-12-27 00:23:09 +0100

    Creation d'une classe entité OperationBancaire

---
+++ src/main/java/ma/elwardi/tp5_elwardi_abderrazzak/entities/OperationBancaire.java
@@ -0,0 +1,62 @@
+package ma.elwardi.tp5_elwardi_abderrazzak.entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Heisenberg
+ */
+@Entity
+public class OperationBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private LocalDateTime dateOperation;
+    private int montant;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDateOperation() {
+        return dateOperation;
+    }
+
+    public void setDateOperation(LocalDateTime dateOperation) {
+        this.dateOperation = dateOperation;
+    }
+
+    public int getMontant() {
+        return montant;
+    }
+
+    public void setMontant(int montant) {
+        this.montant = montant;
+    }
+
+    public OperationBancaire() {
+    }
+
+    public OperationBancaire(String description, int montant) {
+        this.description = description;
+        this.montant = montant;
+        dateOperation = LocalDateTime.now();
+    }
+
+}
